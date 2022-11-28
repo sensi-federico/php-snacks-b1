c2a4d5a586e8ceb53e2ea3baa83c08e2c65ec0a4
sensiz <dev.federicosensi@gmail.com>
2022-11-28 12:21:31 +0100

    final commit

---
+++ snack_1/index.php
@@ -0,0 +1,39 @@
+<!-- ## Snack 1
+
+#1 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+#2 Ogni array avrà una squadra di casa e una squadra ospite, 
+#2 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+#3 Stampiamo a schermo tutte le partite con questo schema.
+
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+$matches = [
+    'Boston Celtics - Chicago Bulls | 102 - 89',
+    'Miami Heat - LA Lakers | 80 - 112',
+];
+
+
+foreach ($matches as $match) {
+    echo $match;
+    echo '<br>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
